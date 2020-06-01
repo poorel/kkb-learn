84376c3b473a083875e07d61e274465078659f09
shenchanghai <shenchanghai@geekplus.com.cn>
2020-06-01 15:34:51 +0800

    Initial commit

---
+++ vue-study-web19/20200529+沈长海+web前端 第一节/components/form/KFormItem.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <label>{{label}}</label>
+    <slot></slot>
+    <p class="error" v-if="error">{{error}}</p>
+  </div>
+</template>
+
+<script>
+import Schema from "async-validator";
+import emitter from "@/mixins/emiiter";
+export default {
+  componentName: "KFormItem",
+  mixins: [emitter],
+  inject: ["form"],
+  props: {
+    label: {
+      type: String,
+      default: ""
+    },
+    prop: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      error: ""
+    };
+  },
+  mounted() {
+    this.prop && this.dispatch('KForm', 'from.addField',[this])
+    this.$on("validate", () => {
+      this.validate();
+    });
+  },
+  methods: {
+    validate() {
+      // 执行校验
+      // 1.获取校验规则
+      const rules = this.form.rules[this.prop];
+      // 2.获取当前值
+      const value = this.form.model[this.prop];
+      // 3.创建校验器
+      const schema = new Schema({
+        // 计算属性
+        [this.prop]: rules
+      });
+      // 4.执行校验
+      return schema.validate({
+        [this.prop]: value
+      }, errors => {
+        if (errors) {
+          // 显示错误信息
+          this.error = errors[0].message
+        } else {
+          // 清空以前可能存在的错误
+          this.error = ''
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ vue-study-web19/20200529+沈长海+web前端 第一节/components/form/KInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- 自定义组件双向绑定：@input,:value -->
+    <input :type="type" :value="value" @input="onInput">
+  </div>
+</template>
+
+<script>
+  import emitter from "@/mixins/emiiter";
+  export default {
+    mixins: [emitter],
+    props: {
+      value: {
+        type: String,
+        default: ''
+      },
+      type: {
+        type: String,
+        default: 'text'
+      },
+    },
+    methods: {
+      onInput(e) {
+        this.$emit('input', e.target.value)
+
+        // 数据变化，需要执行校验
+        //this.$parent.$emit('validate')
+        this.dispatch('KFormItem', 'validate')
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
